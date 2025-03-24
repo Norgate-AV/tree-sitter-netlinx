6c39dc088a9fcda0bb458aa5b4b7e0bf0537e6dc
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-03-24 11:29:32 +0000

    chore(lint-staged): switch to JS config file

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,5 @@
-{
+export default {
     "*?(test|spec).{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
     "*.{css,scss,md,html,json,yml,yaml}": ["prettier --write"],
-    "grammar.js": ["tree-sitter generate", "tree-sitter test"]
-}
+    // "grammar.js": ["tree-sitter generate", "tree-sitter test"],
+};
