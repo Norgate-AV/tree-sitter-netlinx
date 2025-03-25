42aef99adde2e732e0f21be2421a986045f13807
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-03-25 18:30:13 +0000

    feat(queries): add basic folds query file

---
+++ queries/folds.scm
@@ -0,0 +1,49 @@
+;; Compound statements/blocks
+(compound_statement) @fold
+
+;; Struct definitions
+(struct_specifier
+  body: (field_declaration_list) @fold)
+
+;; Function definitions
+(function_definition
+  body: (compound_statement) @fold)
+
+;; Call definitions
+(call_definition
+  body: (compound_statement) @fold)
+
+;; Event handlers
+(button_event_block) @fold
+(channel_event_block) @fold
+(data_event_block) @fold
+
+;; Custom events and timeline events
+(custom_event_definition
+  body: (compound_statement) @fold)
+(timeline_event_definition
+  body: (compound_statement) @fold)
+
+;; Level events
+(level_event_definition
+  body: (compound_statement) @fold)
+
+;; Control flow statements
+(if_statement
+  consequence: (compound_statement) @fold
+  alternative: (else_clause (compound_statement) @fold)?)
+(if_statement
+  alternative: (else_clause (if_statement)) @fold)
+(switch_statement
+  body: (compound_statement) @fold)
+(select_statement) @fold
+(while_statement
+  body: (compound_statement) @fold)
+(for_statement
+  body: (compound_statement) @fold)
+
+;; Initializer lists
+(initializer_list) @fold
+
+;; Comments (only multi-line)
+(comment) @fold
