0192318918842b257e9374d2856d8fe24ab269f3
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-03-25 23:03:38 +0000

    chore(lint-staged): disable precommit test

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -18,7 +18,7 @@ export default {
         return [
             `eslint --fix ${file}`,
             `prettier --write ${file}`,
-            "pnpm test",
+            // "pnpm test",
         ];
     },
 };
