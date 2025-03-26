f5f006d0033dd49bea4e6dbcb5911cd898bdbd65
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-03-26 21:17:55 +0000

    chore(eslint): update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,9 @@
+import { defineConfig } from "eslint/config";
 import treesitter from "eslint-config-treesitter";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default [...treesitter, eslintConfigPrettier];
+export default defineConfig([
+    ...treesitter,
+    eslintConfigPrettier,
+    { ignores: ["src", "bindings", "node_modules"] },
+]);
