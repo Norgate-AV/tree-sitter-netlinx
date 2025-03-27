11dbe7e3d4b3e950b178d9cd1d8d9e49c05812d8
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-03-27 21:12:45 +0000

    refactor(highlights): re-arrange folds and tags

--- queries/folds.scm
+++ queries/folds.scm
@@ -1,10 +1,19 @@
+;; ============================================================================
+;; BASIC BLOCKS
+;; ============================================================================
 ;; Compound statements/blocks
 (compound_statement) @fold
 
+;; ============================================================================
+;; STRUCTURES
+;; ============================================================================
 ;; Struct definitions
 (struct_specifier
   body: (field_declaration_list) @fold)
 
+;; ============================================================================
+;; FUNCTIONS AND CALLS
+;; ============================================================================
 ;; Function definitions
 (function_definition
   body: (compound_statement) @fold)
@@ -13,6 +22,9 @@
 (call_definition
   body: (compound_statement) @fold)
 
+;; ============================================================================
+;; EVENT HANDLERS
+;; ============================================================================
 ;; Event handlers
 (button_event_block) @fold
 (channel_event_block) @fold
@@ -28,6 +40,9 @@
 (level_event_definition
   body: (compound_statement) @fold)
 
+;; ============================================================================
+;; CONTROL FLOW
+;; ============================================================================
 ;; Control flow statements
 (if_statement
   consequence: (compound_statement) @fold
@@ -42,6 +57,9 @@
 (for_statement
   body: (compound_statement) @fold)
 
+;; ============================================================================
+;; MISCELLANEOUS
+;; ============================================================================
 ;; Initializer lists
 (initializer_list) @fold
 
