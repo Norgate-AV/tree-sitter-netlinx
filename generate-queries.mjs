9d89d658565332d553ab7714353c34b5ec386d60
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-03-27 00:49:36 +0000

    feat(scripts): use StringBuilder for generating queries

--- generate-queries.mjs
+++ generate-queries.mjs
@@ -2,29 +2,29 @@
 
 import fs from "node:fs";
 import netlinx from "./netlinx.js";
+import StringBuilder from "string-builder";
 
 const { constants, types, variables, functions } = netlinx;
 
-let queryContent = `;; Auto-generated NetLinx.axi built-ins highlighting
+const builder = new StringBuilder();
 
-;; Functions
-`;
-
-queryContent += `(call_expression
+builder.appendLine(";; Auto-generated NetLinx.axi built-ins");
+builder.appendLine();
+builder.appendLine(";; Functions");
+builder.appendLine(`(call_expression
   function: (identifier) @function.builtin
-  (#match? @function.builtin "(?i)^(${Object.keys(functions).join("|")})$"))
+  (#match? @function.builtin "(?i)^(${Object.keys(functions).join("|")})$"))`);
+builder.appendLine();
+builder.appendLine(";; Constants");
+builder.appendLine(`(identifier) @constant.builtin
+  (#match? @constant.builtin "(?i)^(${Object.keys(constants).join("|")})$")`);
+builder.appendLine();
+builder.appendLine(";; Variables");
+builder.appendLine(`(identifier) @variable.builtin
+  (#match? @variable.builtin "(?i)^(${Object.keys(variables).join("|")})$")`);
+builder.appendLine();
+builder.appendLine(";; Types");
+builder.appendLine(`(type_identifier) @type.builtin
+  (#match? @type.builtin "(?i)^(${Object.keys(types).join("|")})$")`);
 
-;; Constants
-(identifier) @constant.builtin
-  (#match? @constant.builtin "(?i)^(${Object.keys(constants).join("|")})$")
-
-;; Variables
-(identifier) @variable.builtin
-  (#match? @variable.builtin "(?i)^(${Object.keys(variables).join("|")})$")
-
-;; Types
-(type_identifier) @type.builtin
-  (#match? @type.builtin "(?i)^(${Object.keys(types).join("|")})$")
-`;
-
-fs.writeFileSync("./queries/highlights-netlinx.scm", queryContent);
+fs.writeFileSync("./queries/highlights-netlinx.scm", builder.toString());
