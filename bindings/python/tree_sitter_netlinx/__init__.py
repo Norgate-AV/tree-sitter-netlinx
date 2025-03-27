9d1fec5818c66159b0f4aa06f5907f34d00068e2
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-03-27 10:54:54 +0000

    feat(highlights): add disabled highlights for NetLinx/SNAPI axi files

--- bindings/python/tree_sitter_netlinx/__init__.py
+++ bindings/python/tree_sitter_netlinx/__init__.py
@@ -16,6 +16,10 @@ def __getattr__(name):
 
     if name == "HIGHLIGHTS_QUERY":
         return _get_query("HIGHLIGHTS_QUERY", "highlights.scm")
+    # if name == "HIGHLIGHTS_NETLINX_QUERY":
+    #     return _get_query("HIGHLIGHTS_NETLINX_QUERY", "highlights-netlinx.scm")
+    # if name == "HIGHLIGHTS_SNAPI_QUERY":
+    #     return _get_query("HIGHLIGHTS_SNAPI_QUERY", "highlights-snapi.scm")
     # if name == "INJECTIONS_QUERY":
     #     return _get_query("INJECTIONS_QUERY", "injections.scm")
     if name == "LOCALS_QUERY":
@@ -29,6 +33,8 @@ def __getattr__(name):
 __all__ = [
     "language",
     "HIGHLIGHTS_QUERY",
+    "HIGHLIGHTS_NETLINX_QUERY",
+    "HIGHLIGHTS_SNAPI_QUERY",
     # "INJECTIONS_QUERY",
     "LOCALS_QUERY",
     "TAGS_QUERY",
--- generate-queries.mjs
+++ generate-queries.mjs
@@ -2,9 +2,8 @@
 
 import fs from "node:fs";
 import netlinx from "./netlinx.js";
+import snapi from "./snapi.js";
 import StringBuilder from "string-builder";
-
-const { constants, types, variables, functions } = netlinx;
 
 const builder = new StringBuilder();
 
@@ -13,18 +12,32 @@ builder.appendLine();
 builder.appendLine(";; Functions");
 builder.appendLine(`(call_expression
   function: (identifier) @function.builtin
-  (#match? @function.builtin "(?i)^(${Object.keys(functions).join("|")})$"))`);
+  (#match? @function.builtin "(?i)^(${Object.keys(netlinx.functions).join("|")})$"))`);
 builder.appendLine();
 builder.appendLine(";; Constants");
 builder.appendLine(`(identifier) @constant.builtin
-  (#match? @constant.builtin "(?i)^(${Object.keys(constants).join("|")})$")`);
+  (#match? @constant.builtin "(?i)^(${Object.keys(netlinx.constants).join("|")})$")`);
 builder.appendLine();
 builder.appendLine(";; Variables");
 builder.appendLine(`(identifier) @variable.builtin
-  (#match? @variable.builtin "(?i)^(${Object.keys(variables).join("|")})$")`);
+  (#match? @variable.builtin "(?i)^(${Object.keys(netlinx.variables).join("|")})$")`);
 builder.appendLine();
 builder.appendLine(";; Types");
 builder.appendLine(`(type_identifier) @type.builtin
-  (#match? @type.builtin "(?i)^(${Object.keys(types).join("|")})$")`);
+  (#match? @type.builtin "(?i)^(${Object.keys(netlinx.types).join("|")})$")`);
 
 fs.writeFileSync("./queries/highlights-netlinx.scm", builder.toString());
+
+builder.clear();
+builder.appendLine(";; Auto-generated SNAPI.axi built-ins");
+builder.appendLine();
+builder.appendLine(";; Functions");
+builder.appendLine(`(call_expression
+  function: (identifier) @function.builtin
+  (#match? @function.builtin "(?i)^(${Object.keys(snapi.functions).join("|")})$"))`);
+builder.appendLine();
+builder.appendLine(";; Constants");
+builder.appendLine(`(identifier) @constant.builtin
+  (#match? @constant.builtin "(?i)^(${Object.keys(snapi.constants).join("|")})$")`);
+
+fs.writeFileSync("./queries/highlights-snapi.scm", builder.toString());
